c14f7518bcefa011b7ed2acfd44b1a7743186178
seusmer902 <carvajalmanuel902@gmail.com>
2025-09-27 15:34:18 -0500

    se agregaron txt Edad, Estatura

--- capitulo 2/Utilitarios/saludo.js
+++ capitulo 2/Utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar = function(){
-    let nombre = recuperarTexto("txtNombre")
-    let apellido = recuperarTexto("txtApellido")
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad")
+    let estatura = recuperarFloat("txtEstatura")
 }
 
 recuperarTexto = function(idComponente){
@@ -8,4 +10,21 @@ recuperarTexto = function(idComponente){
     let valorIngresado;
     componente = document.getElementById(idComponente);
     valorIngresado = componente.value;
+    return valorIngresado;
+}
+
+recuperarInt = function(idComponente){
+    let valorCaja;
+    let valorEntero;
+    valorCaja = recuperarTexto(idComponente);
+    valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja;
+    let valorFloat;
+    valorCaja = recuperarTexto(idComponente);
+    valorFloat = parseFloat(valorCaja);
+    return valorFloat;
 }
