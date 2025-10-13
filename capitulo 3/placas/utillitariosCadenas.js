7e5baa9027a1c9aa67517bff0bdd16a2bc0aedac
seusmer902 <carvajalmanuel902@gmail.com>
2025-10-13 18:06:41 -0500

    se agregaron las funciones esMayuscula esDigito esGuios

---
+++ capitulo 3/placas/utillitariosCadenas.js
@@ -0,0 +1,31 @@
+esMayuscula = function(caracter){
+  const codigo = caracter.charCodeAt(0);
+
+  if (codigo >= 65 && codigo <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+esDigito = function(caracter){
+
+  const codigo = caracter.charCodeAt(0);
+
+  if (codigo >= 48 && codigo <= 57) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+esGuion = function(caracter) {
+
+  const codigo = caracter.charCodeAt(0);
+
+  if (codigo === 45) {
+    return true;
+  } else {
+    return false;
+  }
+}
